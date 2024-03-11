b9b19fdcd775d6edd25ac0f33e0ab8b5bd438d42
Rohan100304 <161118951+Rohan100304@users.noreply.github.com>
2024-03-11 20:25:28 +0530

    Initial commit

---
+++ check wether the person is eligible to vote.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 19:44:40 2024
+
+@author: Rohan
+"""
+
+age=int(input("enter the age of the person="))
+if age>=18:
+    print("the person is eligibe to vote")
+elif age<18:
+    print ("the person is not eligible to vote")
+else:
+    print("invalid input")
---
+++ untitled18.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 19:44:40 2024
+
+@author: Rohan
+"""
+
