177f7c4f54bc5476def6cbe5cbb8b2862f82497b
Rohan100304 <161118951+Rohan100304@users.noreply.github.com>
2024-04-27 13:12:03 +0530

    Create lsit.py

---
+++ lsit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 14:28:21 2024
+
+@author: Rohan
+"""
+x=[1,9,[2,3,45,6],9.8]
+i=0
+while i<(len(x)):
+    if i==2:
+        for i<
+                print("the value of[",4,"] is=",x[2][i-2])
+                i+=1
+                continue
+    print("the value of[",i,"] is=",x[i])
+    i+=1
+             
+              
+   
+             
